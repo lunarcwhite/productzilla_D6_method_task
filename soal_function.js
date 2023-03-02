ed894d542e623993bf9f2faa0f4d0405c501e332
lunarcwhite <rizkym058@gmail.com>
2023-03-02 21:02:43 +0700

    pengumpulan

---
+++ soal_function.js
@@ -0,0 +1,39 @@
+// 1
+let fungsi1 = (x) => {
+    let nomber1 = x ** 2
+    return `Hasilnya adalah ${nomber1} `
+}
+let nomber1 = fungsi1(10)
+console.log(nomber1)
+
+// 2
+let fungsi2 = (x, y) => {
+    let nomber2 = (x * 2) + (y * 2)
+    return `Hasilnya adalah ${nomber2} `
+}
+let nomber2 = fungsi2(10, 10)
+console.log(nomber2)
+
+// 3
+let fungsi3 = (x, y) => {
+    let nomber3 = (x * y) / 2
+    return `Hasilnya adalah ${nomber3} `
+}
+let nomber3 = fungsi3(10, 10)
+console.log(nomber3)
+
+// 4
+let fungsi4 = (x, y) => {
+    let nomber4 = x * y
+    return `Hasilnya adalah ${nomber4} `
+}
+let nomber4 = fungsi4(10, 10)
+console.log(nomber4)
+
+// 5
+let fungsi5 = (x) => {
+    let nomber5 = 3.14 * 2 * x
+    return `Hasilnya adalah ${nomber5} `
+}
+let nomber5 = fungsi5(10)
+console.log(nomber5)
---
+++ soal_procedure.js
@@ -0,0 +1,10 @@
+//data diri
+function dataDiri(nama, asal, suku){
+    console.log(`Perkenalkan nama saya ${nama}, asal saya dari ${asal}, suku saya adalah suku ${suku}.`)
+}
+
+function kampus(kampus, prodi){
+    console.log(`Saya menempuh pendidikan di ${kampus} mengambil program studi ${prodi}.`)
+}
+dataDiri('Muhamad Rizky', 'Cianjur', 'Sunda') 
+kampus('Universitas Suryakancana', 'Teknik Informatika')
